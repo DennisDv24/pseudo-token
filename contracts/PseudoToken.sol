3cae0a782dc20dcd355a735c1042649a15c54323
DennisDv24 <DennisDv24@proton.me>
2022-11-12 16:12:25 +0100

    api call infra

--- contracts/PseudoToken.sol
+++ contracts/PseudoToken.sol
@@ -3,19 +3,26 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/contracts/interfaces/IUniswapV2Router02.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract PseudoToken is ERC721Enumerable, Ownable {
+contract PseudoToken is 
+	ERC721Enumerable, Ownable, ChainlinkClient
+{
 	
+	uint256 public mintPrice;
+	uint256 public linkFee;
+	mapping (uint256 => string) public tokenIdToTwit;
+
 	IUniswapV2Router02 private uniswapRouter;	
 	address private wethAddr;
 	address private linkAddr;
-	uint256 private linkFee;
-
-	uint256 public mintPrice;
 	address[] private wethLinkPath;
+
+	bytes32 private jobId;
 
 	constructor(
 		string memory tokenName,
@@ -32,26 +39,44 @@ contract PseudoToken is ERC721Enumerable, Ownable {
 		address uniswapRouterAddress,
 		address wethTokenAddress,
 		address linkTokenAddress,
-		address[] memory _wethLinkPath,
-		uint256 linkApiCallFee
+		address[] memory _wethLinkPath
 	) public onlyOwner {
 		uniswapRouter = IUniswapV2Router02(uniswapRouterAddress);
 		wethAddr = wethTokenAddress;
 		linkAddr = linkTokenAddress;
 		wethLinkPath = _wethLinkPath; // Usually [wethAddr, linkAddr]
+	}
+
+	/**
+	 * @dev should be called while deploying
+	 */
+	function configureChainlinkClient(
+		uint256 linkApiCallFee,
+		address chainlinkOracle,
+		bytes32 chainlinkJobId
+	) public onlyOwner {
 		linkFee = linkApiCallFee;
+		require(
+			linkAddr != address(0),
+			"ERROR: Contract integration should be first configured"
+		);
+		setChainlinkToken(linkAddr);
+		setChainlinkOracle(chainlinkOracle);
+		jobId = chainlinkJobId;
 	}
 	
 	/**
 	 * @dev 'msg.value' should be enough to pay for the link fee and
 	 * mint price, so it should be considered from the fron-end
-	 * with a link/weth price feed
+	 * with a link/weth price feed.
+	 * @notice ownership of 'twitterName' should be verified.
 	 */
-	function mint() public payable {
+	function mint(string memory twitterName) public payable {
 		_swapWethForExactLinkFee();
 		require(IERC20(linkAddr).balanceOf(address(this)) == linkFee);
 		require(address(this).balance == mintPrice);
-		// TODO mint
+		_safeMint(msg.sender, totalSupply() + 1);
+		_getPinnedTwit(twitterName);
 	}
 
 	function _swapWethForExactLinkFee() private {
@@ -61,6 +86,17 @@ contract PseudoToken is ERC721Enumerable, Ownable {
 			address(this),
 			2**256 - 1 // Tx deadline
 		);
+	}
+
+	function _getPinnedTwit(string memory twitterName) private {
+		// TODO
+	}
+
+	function fulfill(bytes32 reqId, string memory twit) 
+		public
+		recordChainlinkFulfillment(reqId)
+	{
+		// TODO
 	}
 
 }
